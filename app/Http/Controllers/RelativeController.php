980ca0009ec6b3e1c4a3ea0e3e96dae75e1d88f3
krunaldodiya <kunal.dodiya1@gmail.com>
2018-12-24 13:23:08 +0530

    done

--- app/Http/Controllers/RelativeController.php
+++ app/Http/Controllers/RelativeController.php
@@ -12,25 +12,77 @@ class RelativeController extends Controller
     {
         $authUser = auth()->user();
 
+        if ($request->type === 'send') {
+            return $this->send($request, $authUser);
+        }
+
+        if ($request->type === 'add') {
+            return $this->add($request, $authUser);
+        }
+
+        if ($request->type === 'remove') {
+            return $this->remove($request, $authUser);
+        }
+    }
+
+    public function send($request, $authUser)
+    {
+        $from = $request->from;
+        $to = $request->to;
+
         try {
             Relative::insert([
                 [
                     'user_id' => $authUser['id'],
-                    'from' => $request['from'],
-                    'to' => $request['to'],
+                    'from' => $from,
+                    'to' => $to,
                     'from_relation' => $request['from_relation'],
                     'to_relation' => $request['to_relation'],
                     'status' => false
                 ],
                 [
                     'user_id' => $authUser['id'],
-                    'from' => $request['to'],
-                    'to' => $request['from'],
+                    'from' => $to,
+                    'to' => $from,
                     'from_relation' => $request['to_relation'],
                     'to_relation' => $request['from_relation'],
                     'status' => false
                 ]
             ]);
+
+            $user = User::with('caste', 'sub_caste', 'relatives')->where(['id' => $authUser['id']])->first();
+            return compact('user');
+        } catch (Exception $e) {
+            return ['error' => $e->getMessage()];
+        }
+    }
+
+    public function add($request, $authUser)
+    {
+        $from = $request->from;
+        $to = $request->to;
+
+        try {
+            Relative::where(['from' => $from, 'to' => $to])
+                ->orWhere(['from' => $to, 'to' => $from])
+                ->update(['status' => true]);
+
+            $user = User::with('caste', 'sub_caste', 'relatives')->where(['id' => $authUser['id']])->first();
+            return compact('user');
+        } catch (Exception $e) {
+            return ['error' => $e->getMessage()];
+        }
+    }
+
+    public function remove($request, $authUser)
+    {
+        $from = $request->from;
+        $to = $request->to;
+
+        try {
+            Relative::where(['from' => $from, 'to' => $to])
+                ->orWhere(['from' => $to, 'to' => $from])
+                ->delete();
 
             $user = User::with('caste', 'sub_caste', 'relatives')->where(['id' => $authUser['id']])->first();
             return compact('user');
