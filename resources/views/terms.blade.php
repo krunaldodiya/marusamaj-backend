8b527db058e125479a1390583ec2d6845c8ed422
krunaldodiya <kunal.dodiya1@gmail.com>
2018-12-27 13:20:04 +0530

    done

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,11 +1,184 @@
 @extends($lite ? 'layouts.blank': 'layouts.app')
 
 @section('content')
-  <div style="font-size: 12px">
-    <div style="margin-bottom: 5px">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget arcu dictum varius duis at. Vulputate eu scelerisque felis imperdiet proin fermentum. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Ut placerat orci nulla pellentesque dignissim enim sit amet venenatis. Amet luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Tristique sollicitudin nibh sit amet commodo nulla facilisi. Amet consectetur adipiscing elit duis tristique sollicitudin. Sed risus pretium quam vulputate dignissim suspendisse. Gravida neque convallis a cras semper.</div>
-    <div style="margin-bottom: 5px">Leo vel orci porta non pulvinar. Pharetra et ultrices neque ornare aenean euismod elementum nisi. Pellentesque dignissim enim sit amet venenatis urna cursus eget. Hac habitasse platea dictumst quisque sagittis purus. Id semper risus in hendrerit gravida rutrum quisque. Ultrices eros in cursus turpis. Posuere morbi leo urna molestie at elementum eu facilisis sed. Consequat ac felis donec et odio pellentesque diam. Augue lacus viverra vitae congue eu consequat. Ut morbi tincidunt augue interdum velit euismod. Etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus. Enim praesent elementum facilisis leo vel fringilla est ullamcorper. Vulputate ut pharetra sit amet aliquam. Nunc mi ipsum faucibus vitae aliquet.</div>
-    <div style="margin-bottom: 5px">Elit ut aliquam purus sit amet. Pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies. Habitant morbi tristique senectus et netus et malesuada fames ac. A iaculis at erat pellentesque adipiscing. Vel orci porta non pulvinar neque. Consequat semper viverra nam libero justo laoreet sit amet cursus. Tempus urna et pharetra pharetra massa massa. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget. Malesuada nunc vel risus commodo viverra maecenas accumsan. Lorem dolor sed viverra ipsum nunc aliquet. Sit amet nisl purus in mollis nunc. Tempor commodo ullamcorper a lacus vestibulum. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Nulla facilisi etiam dignissim diam quis enim lobortis scelerisque.</div>
-    <div style="margin-bottom: 5px">Urna et pharetra pharetra massa massa ultricies mi quis. Non odio euismod lacinia at. Pretium lectus quam id leo in vitae. Eget arcu dictum varius duis at. Enim eu turpis egestas pretium aenean pharetra magna. Adipiscing elit ut aliquam purus. Odio euismod lacinia at quis risus sed vulputate odio. In egestas erat imperdiet sed. Mi sit amet mauris commodo quis imperdiet massa. Lorem sed risus ultricies tristique nulla aliquet enim tortor. Ipsum consequat nisl vel pretium lectus quam id. Arcu cursus vitae congue mauris rhoncus aenean vel. Mi tempus imperdiet nulla malesuada. Quam nulla porttitor massa id neque aliquam vestibulum morbi. Urna id volutpat lacus laoreet. Elementum sagittis vitae et leo. Egestas sed tempus urna et pharetra pharetra massa massa ultricies.</div>
-    <div style="margin-bottom: 5px">Ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. Sit amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus. Nibh nisl condimentum id venenatis a condimentum. Facilisis gravida neque convallis a cras semper. Sit amet mauris commodo quis imperdiet massa tincidunt nunc. Nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi. Elementum facilisis leo vel fringilla est ullamcorper. Vel pretium lectus quam id leo in. Massa ultricies mi quis hendrerit. Congue eu consequat ac felis.</div>
+  <div style="font-size: 12px; width: 700px; margin: 0 auto;">
+    <p align="center" class="MsoNormal" style="font-weight: bold">
+        Terms &amp; Conditions
+    </p>
+    
+    <p class="MsoNormal">
+        Welcome to MaruSamaj. MaruSamaj provides Community Networking Services 
+        (the &quot;Services&quot;) pursuant to the terms and conditions set forth below. By 
+        clicking on the [Submit] button, you are agreeing to use the Services in a 
+        manner consistent with the terms and conditions of this Agreement and with all 
+        applicable laws and regulations.
+        <br />
+        <br />
+                1. Modification of Terms of Service: MaruSamaj may change the terms and conditions of 
+        the Service upon which MaruSamaj will notify you of the changes. MaruSamaj may at its sole 
+        discretion from time to time offer you variety of features and facilities for 
+        the service. MaruSamaj however reserves its right to discontinue to offer such features 
+        and facilities for the service at anytime without prior notice.
+        <br />
+        2. Subscriber Conduct: SPAMMING IS PROHIBITED. Spamming includes the 
+        transmission of any form of mail/message/post/comment that can be interpreted as 
+        junk or generated via a distribution list which the recipient has not 
+        specifically requested. MaruSamaj will immediately terminate the service/ any account, 
+        which it believes, in its sole discretion, is transmitting or is otherwise 
+        connected with any spam or other unsolicited bulk content. You agree you are 
+        solely responsible for your content and you acknowledge MaruSamaj acts as a passive 
+        conduit for the distribution and transmission of your data. You represent and 
+        warrant the content you provide to MaruSamaj (a) does not infringe on any third party&#39;s 
+        intellectual property or proprietary rights or rights of publicity or privacy; 
+        (b) does not violate any law, statute, ordinance or regulation; (c) is not 
+        defamatory, trade libelous, pornographic or obscene; or (d) does not contain 
+        viruses or other similar harmful or deleterious programming routines. If you 
+        violate this provision you will be penalised up to and including termination of 
+        your rights to use the service and any other remedies available to MaruSamaj by law.<br />
+        <br />
+        3. Registration &amp; Profile Data: In consideration for the Service, you agree to 
+        provide MaruSamaj current, complete, and accurate registration &amp; profile information 
+        (the &quot;Registration &amp; Profile Data&quot;) as prompted to do by the Service and to 
+        maintain and update this information as required to keep it current, complete 
+        and accurate. You understand the MaruSamaj Service is advertising supported and you 
+        grant MaruSamaj the right to disclose the aggregate Registration &amp; Profile Data to 
+        third parties in connection with marketing the Services. Furthermore, MaruSamaj may 
+        itself use your individual Registration &amp; Profile Data to provide targeting of 
+        advertisements and other Service offers.<br />
+        <br />
+        4. Privacy: Except as provided in Section 2 above, MaruSamaj will not intentionally 
+        monitor or disclose any private message/post/comment or address information 
+        unless required by law, enforcement agencies, regulation or court order. You 
+        agree to resp MaruSamajethe privacy and confidentiality of others.<br />
+        <br />
+        5. Personal Use &amp; Security: You acknowledge your right to use the Service is 
+        personal to you. You agree not to resell or make any commercial use of the 
+        Service without the express consent of CT. You are entirely responsible for not 
+        maintaining the confidentiality of your password and account. You shall be fully 
+        liable for all use of your account, including any unauthorized use of your 
+        account by any third-party.<br />
+        <br />
+        6. Indemnification: You agree to defend, indemnify and hold harmless CT, its 
+        promoters, employees, directors, officers, agents and their successors and 
+        assigns from and against any and all liabilities, damages, losses, costs and 
+        expenses, including attorney&#39;s fees, caused by or arising out of claims based 
+        upon the use of your account, including any claim of libel, defamation, 
+        violation of rights of privacy or publicity, loss of service by other 
+        subscribers and infringement of intellectual property or other rights.<br />
+        <br />
+        7. Terms. This Agreement shall continue in full force and eff MaruSamajefor so long as 
+        you are using the Services. Either you or MaruSamaj may terminate the service at any 
+        time, with or without cause. The provisions of Sections 2, 6 and 12 shall 
+        survive any such termination.
+        <br />
+        <br />
+        MaruSamaj reserves the right, in its sole discretion, to terminate your access to the 
+        services and the related services or any portion thereof at any time, without 
+        notice.
+        <br />
+        <br />
+        MaruSamaj may also terminate or suspend your access to the services for inactivity, 
+        which is defined as failing to log into a particular service for an extended 
+        period of time, as determined by CT.
+        <br />
+        <br />
+        MaruSamaj shall have no obligation to maintain any content or to forward any unread or 
+        unsent messages to you or any third party.<br />
+        <br />
+        8. Disclaimer of Warranties: YOU EXPRESSLY AGREE THE USE OF THE SERVICE IS AT 
+        YOUR SOLE RISK. THE SERVICE IS PROVIDED ON AN &quot;AS IS&quot; AND &quot;AS AVAILABLE&quot; BASIS. 
+        MaruSamaj EXPRESSLY DISCLAIMS ALL WARRANTIES OF ANY KIND, WHETHER EXPRESS OR IMPLIED, 
+        INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS 
+        FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. MaruSamaj MAKES NO WARRANTY THE SERVICES 
+        WILL MEET YOUR REQUIREMENTS, OR THE SERVICES WILL BE UNINTERRUPTED, TIMELY, 
+        SECURE, OR ERROR FREE; NOR DOES MaruSamaj MAKE ANY WARRANTY AS TO THE RESULTS THAT MAY 
+        BE OBTAINED THROUGH THE SERVICE OR THAT DEFECTS IN THE SOFTWARE WILL BE 
+        CORRECTED. YOU UNDERSTAND AND AGREE ANY MATERIAL AND/OR DATA DOWNLOADED OR 
+        OTHERWISE OBTAINED THROUGH THE USE OF THE SERVICE IS DONE AT YOUR OWN DISCRETION 
+        AND RISK AND YOU WILL BE SOLELY RESPONSIBLE FOR ANY DAMAGE TO YOUR COMPUTER 
+        SYSTEMS OR LOSS OF DATA THAT RESULTS FROM THE DOWNLOAD OF SUCH MATERIAL AND/OR 
+        DATA. MaruSamaj MAKES NO WARRANTY REGARDING ANY GOODS OR SERVICES PURCHASED OR OBTAINED 
+        THROUGH THE SERVICE OR ANY TRANSACTIONS ENTERED INTO THROUGH THE SERVICE. NO 
+        ADVICE OR INFORMATION, WHETHER ORAL OR WRITTEN, WHICH YOU OBTAIN FROM CTOR 
+        THROUGH THE SERVICE SHALL CREATE ANY WARRANTY NOT EXPRESSLY MADE HEREIN.
+        <br />
+        <br />
+        YOU ACKNOWLEDGE THAT MaruSamaj SITE MAY CONTAIN LINKS TO OTHER WEB SITES OPERATED BY 
+        THIRD PARTIES (&quot;LINKED SITES&quot;). YOU ACKNOWLEDGE THAT, WHEN YOU CLICK ON A LINK 
+        TO VISIT A LINKED SITE, A FRAME MAY APPEAR THAT CONTAINS THE MaruSamaj LOGO, 
+        ADVERTISEMENTS AND/OR OTHER CONTENT SELECTED BY CT. YOU ACKNOWLEDGE THAT MaruSamaj AND 
+        ITS SPONSORS NEITHER ENDORSE NOR ARE AFFILIATED WITH THE LINKED SITE AND ARE NOT 
+        RESPONSIBLE FOR ANY CONTENT OF ANY LINKED SITE OR ANY LINK CONTAINED IN A LINK 
+        SITE, OR ANY CHANGES OR UPDATES TO SUCH SITES. YOU ALSO ACKNOWLEDGE THAT MaruSamaj IS 
+        PROVIDING THESE LINKS TO YOU ONLY AS A CONVENIENCE.
+        <br />
+        <br />
+        MaruSamaj DOES NOT ENDORSE IN ANYWAY ANY ADVERTISERS/ CONTENTS OF ADVERTISERS ON ITS 
+        WEBPAGES. PLEASE THEREFORE VERIFY THE VERACITY OF ALL INFORMATION ON YOUR OWN 
+        BEFORE UNDERTAKING RELIANCE AND ACTIONING THEREUPON. MaruSamaj SHALL NOT BE RESPONSIBLE 
+        NOR LIABLE FOR ANY CONSEQUENTIAL DAMAGES ARISING ON ACCOUNT OF YOUR RELYING ON 
+        THE CONTENTS OF THE ADVERTISEMENT.<br />
+        <br />
+        9. Limitation of Liability: MaruSamaj SHALL NOT BE LIABLE FOR ANY INDIRECT, INCIDENTAL, 
+        SPECIAL OR CONSEQUENTIAL DAMAGES OR FOR INTERRUPTED COMMUNICATIONS, LOST DATA OR 
+        LOST PROFITS ARISING OUT OF OR IN CONNECTION WITH THIS AGREEMENT, EVEN IF MaruSamaj HAS 
+        BEEN ADVISED OF THE POSSIBILITY OF DAMAGES. SOME JURISDICTIONS DO NOT ALLOW THE 
+        LIMITATION OF LIABILITY SO SOME OF THE ABOVE LIMITATIONS MAY NOT APPLY TO YOU.
+        <br />
+        <br />
+        10. Features &amp; Services: MaruSamaj has and will continue to launch various features and 
+        services. The use of each of the feature and service will have its limits set as 
+        per the technical conditions available. MaruSamaj may add, modify, decrease or delete 
+        limits of the usage of any of its features or services.
+    </p>
+    
+    <p class="MsoNormal">
+        You agree that MaruSamaj may establish limits concerning use of the Services offered on 
+        MaruSamaj Web Site, including without limitation the maximum number of days that 
+        content will be retained by the Services, the maximum number of content
+        <span style="mso-spacerun:yes">&nbsp;</span>that may be sent from or received by an 
+        account on the Services, the maximum size of a content that may be sent from or 
+        received by an account on the Services, the maximum disk space that will be 
+        allotted on CT’s servers on your behalf, and the maximum number of times and 
+        duration you may access any Services in a given period of time.
+        <br />
+        <br />
+        You agree that the size of the content can grow over a period of time. MaruSamaj 
+        reserves the right to limit the rate of growth of the content to a reasonable 
+        limit at its own discretion and without prior notice.         <br />
+        <br />
+        You agree that MaruSamaj has no responsibility or liability for the deletion, 
+        corruption or failure to store any content maintained or transmitted by the 
+        Services. You acknowledge that MaruSamaj reserves the right to log off accounts that 
+        are inactive for an extended period of time.<br />
+        <br />
+        MaruSamaj makes no warranty that any services will be uninterrupted, timely secure or 
+        error-free.<br />
+        <br />
+        11. Proprietary Rights to Content: You acknowledge that content, including but 
+        not limited to text, software, music, sound, photographs, video, graphics or 
+        other material contained in either sponsor advertisements or E-mail distributed, 
+        commercially produced information presented to you by MaruSamaj or CT’s suppliers, is 
+        protected by copyrights, trademarks, service marks, patents or other proprietary 
+        rights and laws. You are permitted to use this content as authorized by the 
+        Service or the Advertiser.<br />
+        <br />
+        12. Miscellaneous: This agreement will be governed by and construed in 
+        accordance with the laws of India without giving eff MaruSamajeto principles of 
+        confl MaruSamajiof laws. If any provision of this Agreement is held to be invalid or 
+        unenforceable, such provision shall be struck and the remaining provisions shall 
+        be enforced. CT’s failure to  MaruSamajawith resp MaruSamajeto a breach by you or others does 
+        not waive its right to  MaruSamajawith resp MaruSamajeto subsequent or similar breaches. This 
+        Agreement sets forth the entire understanding and agreement between you and CT.
+    </p>
+
+    <p>
+        <div class="MsoNormal">MaruSamaj</div>
+        <div class="MsoNormal">Swastik Bunglows</div>
+        <div class="MsoNormal">Raipur Mill Compound</div>
+        <div class="MsoNormal">Rakhial</div>
+        <div class="MsoNormal">Ahmedabad 380018</div>
+        <div class="MsoNormal">India<div>
+    </p>
   </div>
 @endsection
